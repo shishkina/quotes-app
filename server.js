ecf847d5ff4b371ddd4f7e84c704f33997645677
Jason Seminara <jseminara@gmail.com>
2017-08-25 16:48:51 -0700

    exercise 1

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 // Required resources always go first
 // TODO: [1] bring in the express library
+const express = require('express');
 
-
-// start this server on either a provided port, or default to 3000
 // TODO: [2] create a PORT variable that checks the process.env or defaults to 3000
+const PORT = process.env.PORT || 3000;
 
 // TODO: [3] Start up express
+const app = express();
 
 // TODO: [8] set up logging
-
 
 /* ROUTES */
 
 // TODO: [4] Create a GET request handler for '/' (home).
 // send '<h1>Hello World!</h1>' to the user
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+
 
 // WE HAVE LIFT OFF!!
 // TODO: [5] Set up a listener on PORT
 // TODO: [6] Log the message "Server up and listening on port XXXX"
 // TODO: [7] BONUS: log which environment we're in
-
+app.listen(PORT, () => {
+  console.log(`Server up and listening on port ${PORT}, in ${app.get('env')} mode.`);
+});
