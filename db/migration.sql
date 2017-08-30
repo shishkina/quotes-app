86a5a8cd5e400cf0ed89af757280389eb89e761e
shishkina <shishkina.kate@gmail.com>
2017-08-30 18:13:57 -0400

    adds refactoring into 2 tables

--- db/migration.sql
+++ db/migration.sql
@@ -1,13 +1,19 @@
 \c dresselhaus_quotes_dev;
 DROP TABLE IF EXISTS quotes;
+DROP TABLE IF EXISTS genres;
 
+
+CREATE TABLE genres (
+  id SERIAL PRIMARY KEY,
+  genre VARCHAR(255)
+);
 CREATE TABLE quotes (
   id SERIAL PRIMARY KEY,
   content TEXT,
   author VARCHAR(255),
-  genre_type VARCHAR(64),
+  genre_id INTEGER REFERENCES genres(id),
   date_created TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX ON quotes (genre_type);
+CREATE INDEX ON genres (genre);
 CREATE INDEX ON quotes (author);
